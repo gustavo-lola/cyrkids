d84272aff6710c34b33f6bc254cbe3b182a5ba16
Gustavo <gustavolola.dev@gmail.com>
2026-06-20 01:32:40 -0300

    feat: add the logic in the naviagation tabs for this system and i have add the scss for the bottom and the Theme Variables.scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,47 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: "",
+    redirectTo: "tabs",
+    pathMatch: "full",
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
-  },
-  {
-    path: 'crianca-perfil',
-    loadComponent: () => import('./pages/crianca-perfil/crianca-perfil.page').then( m => m.CriancaPerfilPage)
-  },
-  {
-    path: 'historico-vacinal',
-    loadComponent: () => import('./pages/historico-vacinal/historico-vacinal.page').then( m => m.HistoricoVacinalPage)
-  },
-  {
-    path: 'campanhas',
-    loadComponent: () => import('./pages/campanhas/campanhas.page').then( m => m.CampanhasPage)
+    path: "tabs",
+    loadComponent: () =>
+      import("./pages/tabs/tabs.page").then((m) => m.TabsPage),
+    children: [
+      {
+        path: "home",
+        loadComponent: () =>
+          import("./pages/home/home.page").then((m) => m.HomePage),
+      },
+      {
+        path: "crianca-perfil",
+        loadComponent: () =>
+          import("./pages/crianca-perfil/crianca-perfil.page").then(
+            (m) => m.CriancaPerfilPage,
+          ),
+      },
+      {
+        path: "historico-vacinal",
+        loadComponent: () =>
+          import("./pages/historico-vacinal/historico-vacinal.page").then(
+            (m) => m.HistoricoVacinalPage,
+          ),
+      },
+      {
+        path: "campanhas",
+        loadComponent: () =>
+          import("./pages/campanhas/campanhas.page").then(
+            (m) => m.CampanhasPage,
+          ),
+      },
+      {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full",
+      },
+    ],
   },
 ];
