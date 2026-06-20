91ab0e49a6aa190bf8926baac3e150f967c17ed2
Gustavo <gustavolola.dev@gmail.com>
2026-06-20 11:22:47 -0300

    init the modification in the home_page for the user and the verify information

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,38 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: "home",
+    loadComponent: () =>
+      import("./pages/home/home.page").then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
   },
   {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    path: "home",
+    loadComponent: () =>
+      import("./pages/home/home.page").then((m) => m.HomePage),
   },
   {
-    path: 'crianca-perfil',
-    loadComponent: () => import('./pages/crianca-perfil/crianca-perfil.page').then( m => m.CriancaPerfilPage)
+    path: "crianca-perfil",
+    loadComponent: () =>
+      import("./pages/crianca-perfil/crianca-perfil.page").then(
+        (m) => m.CriancaPerfilPage,
+      ),
   },
   {
-    path: 'historico-vacinal',
-    loadComponent: () => import('./pages/historico-vacinal/historico-vacinal.page').then( m => m.HistoricoVacinalPage)
+    path: "historico-vacinal",
+    loadComponent: () =>
+      import("./pages/historico-vacinal/historico-vacinal.page").then(
+        (m) => m.HistoricoVacinalPage,
+      ),
   },
   {
-    path: 'campanhas',
-    loadComponent: () => import('./pages/campanhas/campanhas.page').then( m => m.CampanhasPage)
+    path: "campanhas",
+    loadComponent: () =>
+      import("./pages/campanhas/campanhas.page").then((m) => m.CampanhasPage),
   },
 ];
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import {
+  IonAvatar,
+  IonBadge,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonLabel,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/angular/standalone";
+import { addIcons } from "ionicons";
+import { bellOutline } from "ionicons/icons";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  selector: "app-home",
+  templateUrl: "./home.page.html",
+  styleUrls: ["./home.page.scss"],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonAvatar,
+    IonLabel,
+    IonIcon,
+    IonButton,
+    IonBadge,
+  ],
 })
 export class HomePage implements OnInit {
+  user = {
+    name: "Maria",
+    avatar: "https://i.pravatar.cc/150?img=147",
+  };
 
-  constructor() { }
+  kids = [
+    {
+      name: "",
+      avatar: "",
+    },
+    {
+      name: "",
+      avatar: "",
+    },
+    {
+      name: "",
+      avatar: "",
+    },
+    {
+      name: "",
+      avatar: "",
+    },
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
